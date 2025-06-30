ef49f68ccd22dbbce09880487f55c2790e70c465
RMT <mumer@rmt-pk.com>
2025-06-30 17:41:05 +0500

    jwt

--- djangobackend/demo/urls.py
+++ djangobackend/demo/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import login_view, user_files_view
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
+
 
 urlpatterns = [
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', login_view, name='login'),
     path('dashboard/', user_files_view, name='user-files')
 ]
--- djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/settings.py
@@ -39,7 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'demo',
     'rest_framework',
+    'rest_framework_simplejwt'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
